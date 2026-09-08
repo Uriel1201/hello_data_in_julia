3e432c7c6f726ec4e50b5d501ccb2a5ca266f739
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-08 22:37:26 +0000

    Add data processing modules

--- src/database_setup/sqlite.jl
+++ src/database_setup/sqlite.jl
@@ -19,4 +19,17 @@ function get_conn(f::Function, db_path::String = ":memory:", mode::String = "def
         SQLite.close(db)
     end
 end
-end # module MySQLite
+
+
+"""
+    sqlite_ti_arrow(conn::SQLite.DB, query::String, output_file::String, chunk_size::Int64)
+"""
+function sqlite_to_arrow(conn::SQLite.DB, query::String, output_file::String, chunk_size::Int64)::Nothing
+    file_path = joinpath("data/arrow", "$output_file.arrow")
+    result = DBInterface.execute(conn, query)
+    open(Arrow.Writer, file_path) do writer
+        for chunk in Iterators.partition(result, chunk_size)
+            Arrow.write(writer, collect(chunk))
+        end
+    end
+endend # module MySQLite
