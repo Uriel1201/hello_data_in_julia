0943c32c274cf3aa3c8e6a95336d0e0a9d263db9
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-08 08:57:15 +0000

    Add data processing modules

--- src/database_setup/sqlite.jl
+++ src/database_setup/sqlite.jl
@@ -1,7 +1,17 @@
-module MySqlite
+module MySQLite
 using SQLite
 
-function get_conn(dbs::String):: Nothing 
-    print("hola")
+function get_conn(dbs::String=":memory:", mode::String="default")::SQLite.DB
+    if dbs == ":memory:"
+        return SQLite.DB()
+    end
+    path = joinpath("data", "$dbs.sqlite")
+    if ispath(path)
+        uri = "file:$path?mode=$mode"
+        return SQLite.DB(uri)
+    else
+        uri = "file:$path"
+        return SQLite.DB(uri)
+    end
 end
-end #module
+end # module MySQLite
