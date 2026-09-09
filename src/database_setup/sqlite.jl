119969352133c51be55ff6c84ecd5d2d1ac822ce
Uriel1201 <dante.1201mgie@gmail.com>
2026-09-09 06:37:16 +0000

    Add data processing modules

--- src/database_setup/sqlite.jl
+++ src/database_setup/sqlite.jl
@@ -25,7 +25,11 @@ end
 """
     sqlite_ti_arrow(conn::SQLite.DB, query::String, output_file::String)
 """
-function sqlite_to_arrow(conn::SQLite.DB, query::String, output_file::String)::Nothing
+function sqlite_to_arrow(
+    conn::SQLite.DB,
+    query::String,
+    output_file::String,
+)::Nothing
 
     file_path = joinpath("data/arrow", "$output_file.arrow")
 
@@ -38,7 +42,7 @@ function sqlite_to_arrow(conn::SQLite.DB, query::String, output_file::String)::N
         for row in result
             push!(batch, NamedTuple(row))
 
-            if length(batch) == 2
+            if length(batch) == 5
                 table = Tables.columntable(batch)
                 Arrow.write(writer, table)
 
