ac703371a37efd60ce8b80115fcf983dcc66b2b0
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-10 04:44:47 -0600

    Update my_tables.jl

--- data/my_tables.jl
+++ data/my_tables.jl
@@ -6,18 +6,14 @@ using DBInterface, SQLite, Tables
 function create_users_01(conn::SQLite.DB, list_table::Vector{String})::Nothing
 
     if !("users_01" in list_table)
-
         schema = Tables.Schema((:user_id, :action, :dates), (Int32, String, String))
         SQLite.createtable!(conn, "users_01", schema, temp = false)
 
         columns = join(schema.names, " | ")
         _type = join(schema.types, " | ")
         @info "users_01 created:" columns _type
-
     else
-
         @info "users_01 already exists"
-
     end
     nothing
 end # create_users_01
