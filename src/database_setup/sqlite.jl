4567230475d336cda6ceddf99341e358da8e88ba
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2026-09-11 14:19:36 -0600

    Update sqlite.jl

--- src/database_setup/sqlite.jl
+++ src/database_setup/sqlite.jl
@@ -106,16 +106,12 @@ end # mytable
     create_table(conn::SQLite.DB, table_name::String, schema::Tables.Schema) -> MyTable
 """
 function create_table(conn::SQLite.DB, table_name::String, schema::Tables.Schema)::MyTable
-    list_table = my_tables(conn)
-    if !(table_name in list_table)
-        SQLite.createtable!(conn, table_name, schema, temp = false)
+    SQLite.createtable!(conn, table_name, schema, temp = false)
+    
+    columns = join(schema.names, " | ")
+    _type = join(schema.types, " | ")
+    @info "$table_name created: " columns _type
 
-        columns = join(schema.names, " | ")
-        _type = join(schema.types, " | ")
-        @info "$table_name created: " columns _type
-    else
-        @info "$table_name already exists"
-    end
     return mytable(table_name, schema)
 end # create_table
 
@@ -124,19 +120,14 @@ end # create_table
     ingest_data(conn::SQLite.DB, my_table::MyTable, data) -> Nothing 
 """
 function ingest_data(conn::SQLite.DB, my_table::MyTable, data)::Nothing
-    list_table = my_tables(conn)
-    if (my_table.name in list_table)
-        n = length(first(data))
-        placeholders = join(["(" * join(fill("?", n), ", ") * ")" for _ in data], ", ")
-
-        query = "INSERT INTO $(my_table.name) $(my_table.stmt_columns) VALUES $placeholders"
-        stmt = SQLite.Stmt(conn, query)
-        params = collect(Iterators.flatten(data))
-        DBInterface.execute(stmt, params)
-        @info "ingestion completed"
-    else
-        error("$my_table.name does not exist")
-    end
+    n = length(first(data))
+    placeholders = join(["(" * join(fill("?", n), ", ") * ")" for _ in data], ", ")
+    
+    query = "INSERT INTO $(my_table.name) $(my_table.stmt_columns) VALUES $placeholders"
+    stmt = SQLite.Stmt(conn, query)
+    params = collect(Iterators.flatten(data))
+    DBInterface.execute(stmt, params)
+    @info "ingestion completed"
     nothing
 end # ingest_data
 end # module MySQLite
